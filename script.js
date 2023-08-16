21daca0e16009705e6e5083ad930d7c93b59dd23
FIRST_NAME LAST_NAME <drako1224dragon@gmail.com>
2023-08-16 20:13:36 +0200

    feat: add external links

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import { cards } from "./data/cards";
-import { projects } from "./data/projects";
-import Masonry from 'masonry-layout';
+import { cards } from "./data/cards.js";
+import { projects } from "./data/projects.js";
+// import Masonry from 'masonry-layout';
 
 (function() {
     const container = document.querySelector('.cards__cont');
@@ -14,7 +14,7 @@ import Masonry from 'masonry-layout';
             const icon = document.createElement("div");
             icon.classList.add("card__icon");
             card.appendChild(icon);
-            icon.style.backgroundImage = `url(${cards[i].img})`; 
+            icon.style.backgroundImage = cards[i]; 
 
             const name = document.createElement("span");
             name.classList.add("card__name");
@@ -28,17 +28,46 @@ import Masonry from 'masonry-layout';
 
             const button = document.createElement("div");
             button.classList.add("card__button");
+            button.setAttribute("id", cards[i].id);
             card.appendChild(button);
             button.innerHTML = cards[i].button;
 
             const buttonArrow = document.createElement("div");
             buttonArrow.classList.add("btn__arrow");
             button.appendChild(buttonArrow);
-            buttonArrow.style.backgroundImage = `url(${cards[i].arrow})`;
+            buttonArrow.style.backgroundImage = cards[i].arrow;
         }
     }
     loadCards(cards);
 })();
+
+const linkOpen = (link) => {
+    window.open(link);
+}
+
+const logo = document.getElementById('logo');
+const inst = document.getElementById('inst__btn');
+const visual = document.getElementById("1");
+const real = document.getElementById("2");
+logo.addEventListener('click', () => linkOpen('https://adrespect.pl/'));
+inst.addEventListener('click', () => linkOpen('https://www.instagram.com/'));
+real.addEventListener('click', () => linkOpen('https://www.instagram.com/'));
+visual.addEventListener('click', () => linkOpen('https://www.instagram.com/'));
+
+function openEmail() {
+    const mailtoLink = 'mailto:';
+    window.location.href = mailtoLink;
+}
+
+const contactFooter = document.getElementById('contact__footer');
+const contactHeader = document.getElementById('contact__header');
+const email = document.getElementById('email');
+const contactBtn = document.getElementById('contact__button');
+
+email.addEventListener('click', () => openEmail());
+contactHeader.addEventListener('click', () => openEmail());
+contactFooter.addEventListener('click', () => openEmail());
+contactBtn.addEventListener('click', () => openEmail());
 
 (function() {
     const container = document.querySelector('.projects');
@@ -48,18 +77,19 @@ import Masonry from 'masonry-layout';
             const project = document.createElement("div");
             project.classList.add("project__card");
             container.appendChild(project);
-            project.style.backgroundImage = `url(${projects[i].img})`; 
+            project.style.backgroundImage = projects[i].picture; 
 
             const name = document.createElement("span");
             name.classList.add("project__name");
             project.appendChild(name);
             name.innerHTML = projects[i].name;
         }
-        const masonry = new Masonry(container, {
-            itemSelector: '.project__card',
-            columnWidth: '.project__card',
-            gutter: 10,
-        });
+        // const masonry = new Masonry(container, {
+        //     itemSelector: '.project__card',
+        //     columnWidth: '.project__card',
+        //     gutter: 10,
+        // });
     }
     loadProjects(projects);
 })();
+
