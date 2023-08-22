4b1a46839cfc2ec5c3cf7278a0a8bb15f4d6c055
FIRST_NAME LAST_NAME <drako1224dragon@gmail.com>
2023-08-22 03:05:06 +0200

    feat: add hamburger menu

--- script.js
+++ script.js
@@ -279,3 +279,18 @@ function loadSlider(cards) {
 }
 
 loadSlider(sliderCards);
+
+const hamburger = document.querySelector(".hamburger");
+const line1 = document.querySelector(".line1");
+const line2 = document.querySelector(".line2");
+const line3 = document.querySelector(".line3");
+const nav = document.getElementById("nav");
+const navigation = document.getElementById("navigation");
+
+hamburger.onclick = () => {
+  nav.classList.toggle("open");
+  line1.classList.toggle("change");
+  line2.classList.toggle("change");
+  line3.classList.toggle("change");
+  navigation.classList.toggle("display");
+};
